ace3682600496051bbdd85125b558e7c086c0acc
armenkhachatryan <armen.khachatryan@polymorphic.io>
2020-06-17 17:42:32 +0400

    added reducers for first list

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Header from '../Header';
 import './styles.css';
 import Titles from '../Title';
 import InputList from '../InputList';
+import store from '../../redux/store';
+
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const App = () => {
   return (
-    <>
-      <div className='app'>
-        <Header />
-        <Titles />
-        <InputList />
-      </div>
-    </>
+    <Provider store={store}>
+      <DndProvider backend={HTML5Backend}>
+        <div className='app'>
+          <Header />
+          <Titles />
+          <InputList />
+        </div>
+      </DndProvider>
+    </Provider>
   );
 };
 
--- src/components/InputList/index.js
+++ src/components/InputList/index.js
@@ -1,103 +1,169 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { DndProvider, useDrop } from 'react-dnd';
+// import {} from 'redux'
+import { connect } from 'react-redux';
+import { addInputField, removeInputField } from '../../redux/actions/';
+import DraggableInput from '../DragAndDrop';
+import { ItemTypes } from '../Constants';
 import './styles.css';
-class InputList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      prosList: [{ id: 1, value: '' }],
-      consList: [{ id: 1, value: '' }],
-      value: ''
-    };
-  }
 
-  handleProsListChange = (event, index, callback) => {
+function mapStateToProps(state) {
+  return {
+    prosList: state.prosList,
+    consList: state.consList
+  };
+}
+
+const mapDispatchToProps = {
+  addInputField,
+  removeInputField
+};
+
+const InputList = props => {
+  const [state, setState] = useState({
+    prosList: [{ id: 1, value: '' }],
+    consList: [{ id: 1, value: '' }]
+  });
+
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: ItemTypes.INPUT
+  });
+
+  const handleProsListChange = (event, index) => {
     event.preventDefault();
-    let prosList = this.state.prosList;
+    let prosList = props.prosList;
     prosList[index].id = index + 1;
     prosList[index].value = event.target.value;
 
-    this.setState({ prosList }, () => {
-      console.log('proslist', this.state.prosList[index]);
-      callback();
-    });
+    console.log('hqiudhuhuhhhh', prosList[index].value);
+
+    // setState(prevState => ({ ...prevState, prosList }));
+    props.addInputField({ ...(index + 1), ...event.target.value });
   };
 
-  handleConsListChange = (event, index, callback) => {
+  const handleConsListChange = (event, index) => {
     event.preventDefault();
-    let consList = this.state.consList;
+    let consList = props.consList;
     consList[index].id = index + 1;
     consList[index].value = event.target.value;
 
-    this.setState({ consList }, () => {
-      callback();
-    });
+    setState(prevState => ({ ...prevState, consList }));
   };
 
-  render() {
-    return (
+  const dragProsListItem = (item, index) => {
+    let prosList = props.prosList;
+    let consList = props.consList;
+    for (let key in consList) {
+      if (!consList[key].value) {
+        consList.pop();
+      }
+    }
+    consList.push(item);
+    consList.push({ id: item.id + 1, value: '' });
+    if (prosList.length !== 1) {
+      prosList.splice(index, 1);
+      setState(prevState => ({
+        ...prevState,
+        prosList,
+        consList
+      }));
+    }
+  };
+
+  return (
+    <div ref={drop}>
       <div className='flex-container'>
         <ol>
-          {this.state.prosList.map((item, index) => {
+          {props.prosList.map((item, index) => {
             return (
               <li key={index}>
-                <input
-                  type='text'
-                  onChange={event => {
-                    this.handleProsListChange(event, index, () => {
-                      let prosList = this.state.prosList;
-                      if (
-                        item.id === this.state.prosList.length &&
-                        item.value
-                      ) {
-                        prosList.push({ id: item.id + 1, value: '' });
-                        this.setState({ prosList });
-                      }
-                      if (!item.value) {
-                        prosList.splice(index, 1);
-                        this.setState({ prosList });
-                      }
-                    });
-                  }}
+                <DraggableInput
                   value={item.value}
-                ></input>
+                  onChange={event => {
+                    handleProsListChange(event, index);
+                    let prosList = props.prosList;
+                    if (item.id === props.prosList.length && item.value) {
+                      prosList.push({ id: item.id + 1, value: '' });
+                      // props.addInputField(item.id + 1, event.target.value);
+                    }
+                    if (!item.value) {
+                      // prosList.splice(index, 1);
+                      props.removeInputField({
+                        ...index,
+                        ...event.target.value
+                      });
+                    }
+                  }}
+                  onEndDrag={() => {
+                    dragProsListItem(item, index);
+                  }}
+                />
               </li>
             );
           })}
         </ol>
-
+        {/* 
         <div className='vertical-line' />
 
         <ol>
-          {this.state.consList.map((item, index) => {
+          {state.consList.map((item, index) => {
             return (
               <li key={index}>
-                <input
-                  type='text'
+                <DraggableInput
                   onChange={event => {
-                    this.handleConsListChange(event, index, () => {
-                      let consList = this.state.consList;
-                      if (
-                        item.id === this.state.consList.length &&
-                        item.value
-                      ) {
-                        consList.push({ id: item.id + 1, value: '' });
-                        this.setState({ consList });
+                    handleConsListChange(event, index);
+                    let consList = state.consList;
+                    if (item.id === state.consList.length && item.value) {
+                      consList.push({ id: item.id + 1, value: '' });
+                      setState(prevState => ({ ...prevState, consList }));
+                    }
+                    if (!item.value) {
+                      consList.splice(index, 1);
+                      setState(prevState => ({ ...prevState, consList }));
+                    }
+                  }}
+                  onEndDrag={() => {
+                    let prosList = state.prosList;
+                    let consList = state.consList;
+                    for (let key in prosList) {
+                      if (!prosList[key].value) {
+                        prosList.pop();
                       }
-                      if (!item.value) {
-                        consList.splice(index, 1);
-                        this.setState({ consList });
-                      }
-                    });
+                    }
+                    prosList.push(item);
+                    prosList.push({ id: item.id + 1, value: '' });
+                    if (consList.length !== 1) {
+                      consList.splice(index, 1);
+                      setState(prevState => ({
+                        ...prevState,
+                        consList,
+                        prosList
+                      }));
+                    }
                   }}
                   value={item.value}
                 />
               </li>
             );
           })}
-        </ol>
+        </ol> */}
       </div>
-    );
-  }
-}
+      {/* {isOver && (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '100%',
+            width: '100%',
+            zIndex: 1,
+            opacity: 0.5,
+            backgroundColor: 'yellow'
+          }}
+        />
+      )} */}
+    </div>
+  );
+};
 
-export default InputList;
+export default connect(mapStateToProps, mapDispatchToProps)(InputList);
