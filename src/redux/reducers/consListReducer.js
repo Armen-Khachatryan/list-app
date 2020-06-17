04032042492513f2b07761ff54c09fbfe1d40a88
armenkhachatryan <armen.khachatryan@polymorphic.io>
2020-06-17 21:07:19 +0400

    clean up

--- src/redux/reducers/consListReducer.js
+++ src/redux/reducers/consListReducer.js
@@ -2,10 +2,8 @@ const consListreducer = (consList = [{ id: 1, value: '' }], action) => {
   if (action.type === 'ADD_ITEM_TO_CONS') {
     return [...consList, action.payload];
   } else if (action.type === 'REMOVE_ITEM_FROM_CONS') {
-    // if (!action.payload.value && action.payload.id !== consList.length) {
     consList = consList.filter(element => element.value !== '');
     return [...consList, { id: action.payload.id + 1, value: '' }];
-    // }
   }
   return consList;
 };
--- src/redux/reducers/prosListReducer.js
+++ src/redux/reducers/prosListReducer.js
@@ -3,10 +3,8 @@ const prosListreducer = (prosList = [{ id: 1, value: '' }], action) => {
   if (action.type === 'ADD_ITEM_TO_PROS') {
     return [...prosList, action.payload];
   } else if (action.type === 'REMOVE_ITEM_FROM_PROS') {
-    // if (!action.payload.value && action.payload.id !== prosList.length) {
     prosList = prosList.filter(element => element.value !== '');
     return [...prosList, { id: action.payload.id + 1, value: '' }];
-    // }
   }
   return prosList;
 };
